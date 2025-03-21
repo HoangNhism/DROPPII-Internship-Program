a623be31623db1fa4b0b8da66ec46a32eef9c78c
Hoangnhism <hoangtranminh2003@gmail.com>
2025-03-21 18:04:23 +0700

    push Droppii project

---
+++ src/test/java/com/example/to_do_list_application/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_do_list_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
